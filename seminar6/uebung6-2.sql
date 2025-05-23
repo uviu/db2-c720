a6cfcf3471f73a1c008773cf411e57289354dfbe
Luca Schneider <lucamarlonschneider@gmail.com>
2025-05-23 09:58:58 +0200

    fix: works, ignore warnings

--- seminar6/uebung6-2.sql
+++ seminar6/uebung6-2.sql
@@ -5,3 +5,12 @@ insert into hochschule
 values ('IM', 'Karl-Liebknecht-Straße. 145', SG_TABLE_TYPE(SG('INB', 6, 'B.Sc.')));
 COMMIT;
 
+insert into table ( select h.studgaenge
+                    from hochschule h
+                    where fbez = 'IM')
+values (SG('MIB', 6, 'B.Sc.'));
+
+insert into table ( select h.studgaenge
+                    from HOCHSCHULE h
+                    where FBEZ = 'ING')
+values (SG('EIB', 6, 'B.Sc.'));
